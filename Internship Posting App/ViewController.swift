38a63acad803622213fcb06855968f98a115f042
Bailey Ilagan <ilaganbgm@gmail.com>
2017-07-17 20:32:07 -1000

    Initial Commit

---
+++ Internship Posting App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Internship Posting App
+//
+//  Created by Bailey Ilagan on 7/17/17.
+//  Copyright © 2017 Bailey Ilagan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
